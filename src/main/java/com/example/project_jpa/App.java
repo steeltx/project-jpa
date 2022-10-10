c7c3bac639ccfff38d73e8c52c3af7edb88b289b
steeltx <realsteel.gnu@gmail.com>
2022-10-10 00:24:46 -0500

    Versión inicial del proyecto JPA

---
+++ src/main/java/com/example/project_jpa/App.java
@@ -0,0 +1,13 @@
+package com.example.project_jpa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
